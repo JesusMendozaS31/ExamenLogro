0a9cb736efdc68ea48808ef4f5fcce8aff4584be
JesusMendozaS31 <jms31gameplays@gmail.com>
2025-06-13 17:09:45 -0500

    Add files via upload

--- Examen_Logro_MendozaSernaque/bd_pruebalogro2025.sql
+++ Examen_Logro_MendozaSernaque/bd_pruebalogro2025.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-10-2024 a las 16:26:39
+-- Tiempo de generación: 14-06-2025 a las 00:08:29
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bd_pruebalogro2025`
 --
-CREATE DATABASE IF NOT EXISTS `bd_pruebalogro2025` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `bd_pruebalogro2025`;
 
 -- --------------------------------------------------------
 
@@ -45,30 +43,8 @@ CREATE TABLE `denuncias` (
 --
 
 INSERT INTO `denuncias` (`id`, `titulo`, `descripcion`, `ubicacion`, `estado`, `ciudadano`, `telefono_ciudadano`, `fecha_registro`) VALUES
-(1, 'Robo de bicicletaaa', 'Se reporta el robo de una bicicleta en el parque central', 'Parque Central', 'Parque Central', 'Juan Pérez', '987654321', '2024-10-30 10:22:26'),
-(2, 'Vandalismo en parque', 'Grafitis realizados en las paredes del parque', 'Parque Los Olivos', 'en proceso', 'Luis Torres', '912345678', '2024-10-30 10:22:26'),
-(3, 'Basura acumulada', 'Acumulación de basura en la calle principal', 'Calle Mayor 45', 'pendiente', 'Ana Ramírez', '923456789', '2024-10-30 10:22:26'),
-(4, 'Maltrato animal', 'Perro abandonado en condiciones deplorables', 'Avenida Lima 123', 'resuelta', 'Clara Gutiérrez', '934567890', '2024-10-30 10:22:26'),
-(6, 'Bache en carretera', 'Hueco grande que afecta el tránsito de vehículos', 'Avenida del Sol', 'pendiente', 'María López', '956789012', '2024-10-30 10:22:26'),
-(7, 'Fuga de agua', 'Fuga de agua en la esquina de la avenida', 'Esquina Avenida y Calle 5', 'en proceso', 'Pedro Martínez', '967890123', '2024-10-30 10:22:26'),
-(8, 'Obstrucción de acera', 'Autos estacionados bloqueando el paso peatonal', 'Calle 12', 'resuelta', 'Sofía García', '978901234', '2024-10-30 10:22:26'),
-(9, 'Fallo en alumbrado', 'Poste de luz no funciona en la esquina', 'Esquina Calle 9 y 10', 'pendiente', 'José Fernández', '989012345', '2024-10-30 10:22:26'),
-(10, 'Contaminación de río', 'Derrame de residuos en el río local', 'Río Chillón', 'pendiente', 'Elena Rojas', '900123456', '2024-10-30 10:22:26'),
-(11, 'Venta de alcohol a menores', 'Venta ilegal de alcohol a menores en la tienda', 'Tienda de Barrio', 'en proceso', 'Luis García', '911234567', '2024-10-30 10:22:26'),
-(12, 'Acoso en espacio público', 'Persona reporta acoso en el parque', 'Parque Primavera', 'pendiente', 'Andrea Mendoza', '922345678', '2024-10-30 10:22:26'),
-(13, 'Perro agresivo', 'Perro suelto que ha atacado a transeúntes', 'Callejón sin salida', 'resuelta', 'Ramiro Soto', '933456789', '2024-10-30 10:22:26'),
-(14, 'Escombros en la calle', 'Escombros abandonados en plena vía pública', 'Callejón del Norte', 'pendiente', 'Nora Vázquez', '944567890', '2024-10-30 10:22:26'),
-(15, 'Tala ilegal de árboles', 'Árboles talados sin permiso en la plaza', 'Plaza Principal', 'en proceso', 'Pablo Ruiz', '955678901', '2024-10-30 10:22:26'),
-(16, 'Personas sin mascarilla', 'Personas incumpliendo medidas sanitarias', 'Centro Comercial', 'pendiente', 'Patricia Ortiz', '966789012', '2024-10-30 10:22:26'),
-(17, 'Incendio forestal', 'Pequeño incendio en la zona forestal', 'Bosque Municipal', 'resuelta', 'Oscar Chávez', '977890123', '2024-10-30 10:22:26'),
-(18, 'Venta de productos ilegales', 'Venta de productos sin regulación', 'Mercado San Juan', 'pendiente', 'Inés Peña', '988901234', '2024-10-30 10:22:26'),
-(19, 'Calle bloqueada', 'Obras de construcción sin señalización adecuada', 'Avenida Principal', 'en proceso', 'Miguel Palacios', '999012345', '2024-10-30 10:22:26'),
-(20, 'Contaminación sonora', 'Vehículo con alto volumen de música', 'Calle 8', 'pendiente', 'Ángela Quispe', '910123456', '2024-10-30 10:22:26'),
-(21, 'Fallo de semáforo', 'Semáforo no funciona en intersección', 'Intersección Avenida Perú', 'pendiente', 'Fabiola García', '931234567', '2024-10-30 10:22:26'),
-(22, 'Vehículo abandonado', 'Carro abandonado en la calle por más de 3 meses', 'Calle La Mar 150', 'en proceso', 'Hugo Díaz', '941234567', '2024-10-30 10:22:26'),
-(23, 'Agua estancada', 'Acumulación de agua estancada que atrae mosquitos', 'Calle 13', 'pendiente', 'Silvia Pérez', '951234567', '2024-10-30 10:22:26'),
-(24, 'Fuga de gas', 'Se percibe olor a gas en el edificio', 'Edificio Los Pinos', 'resuelta', 'Tomás Ríos', '961234567', '2024-10-30 10:22:26'),
-(25, 'Robo en la Plaza Centralll', 'uwu', 'uwu', 'en_proceso', 'UWU', 'UWU', '2024-10-30 10:25:31');
+(1, 'Robo de Bicicleta BMX', 'Se reporta el robo de una bicicleta en el parque central', 'Parque Central Mocupe', 'Parque Central', 'Carlos Alcaraz', '965465734', '2025-06-13 16:25:37'),
+(28, 'Robo de telefono', 'Iphone 16 pro max', 'PIMENTEL PARQUE', 'pendiente', 'Martin Montes', '902743862', '2025-06-13 16:26:28');
 
 --
 -- Índices para tablas volcadas
@@ -88,7 +64,7 @@ ALTER TABLE `denuncias`
 -- AUTO_INCREMENT de la tabla `denuncias`
 --
 ALTER TABLE `denuncias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
